c6af3ded76147f493fe2fc5b7080b71321c754d1
psy0821-k <p49417875@gmail.com>
2026-06-09 09:20:50 +0900

    fix: 출결 상태 위치 변경 직원목록 => 근태관리

--- src/components/userCard/userInfo.tsx
+++ src/components/userCard/userInfo.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import TableComponent from '../table/tableComponent';
 
 const UserInfo = () => {
   return (
     <div>
-      <div>오늘 지각자</div>
       <TableComponent />
     </div>
   );
